eff66c9caa03372f7f13b20e9671c34a9fe93f0f
Abanoub Medhat <abanoub.medhat.seif@gmail.com>
2024-11-02 00:07:01 +0200

    fix navbar errors

---
+++ src/components/user/SidePanel.tsx
@@ -0,0 +1,48 @@
+import { t } from "i18next";
+import { ReactNode } from "react";
+
+interface MenuItem {
+  title: string;
+  icon: ReactNode;
+  component: ReactNode;
+}
+
+interface SidePanelProps {
+  menuItems: MenuItem[];
+  setActivePanel: (activePanel: string) => void;
+}
+
+export default function SidePanel({
+  menuItems,
+  setActivePanel,
+}: SidePanelProps) {
+  return (
+    <div className="bg-white w-80 min-h-screen start-0 top-0 p-10 ps-16 max-laptop:hidden">
+      <div className="border-b-2 border-gray-200 pb-6">
+        <p className="font-bold text-gray-700">{t("hala")}</p>
+        <p className="font-light text-xs text-gray-400 break-words">
+          abanoub.medhat.seif@gmail.com
+        </p>
+      </div>
+      <div className="py-5 border-b-2 border-gray-200">
+        {menuItems.map((item) => (
+          <div
+            key={item.title}
+            className="flex gap-3 hover:bg-gray-200 active:bg-gray-300 p-5 transition-all duration-200 rounded-xl cursor-pointer"
+            onClick={() => {
+              setActivePanel(item.title);
+            }}
+          >
+            <span>{item.icon}</span>
+            <p className=" text-gray-400 ">{t(item.title)}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <button className="py-5 text-gray-400 hover:text-gray-500">
+          {t("logout")}
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/pages/users/UserDashboard.tsx
+++ src/pages/users/UserDashboard.tsx
@@ -1,4 +1,3 @@
-import { t } from "i18next";
 import { orange } from "@mui/material/colors";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
@@ -9,6 +8,7 @@ import Profile from "./UserDashboardComponents/Profile";
 import Wishlist from "./UserDashboardComponents/Wishlist";
 import Orders from "./UserDashboardComponents/Orders";
 import Address from "./UserDashboardComponents/Address";
+import SidePanel from "../../components/user/SidePanel";
 
 const menuItems = [
   {
@@ -42,33 +42,10 @@ export default function UserDashboard() {
   return (
     <div className="flex">
       {/* Left Panel */}
-      <div className="bg-white w-80 min-h-screen start-0 top-0 p-10 ps-16 max-laptop:hidden">
-        <div className="border-b-2 border-gray-200 pb-6">
-          <p className="font-bold text-gray-700">{t("hala")}</p>
-          <p className="font-light text-xs text-gray-400 break-words">
-            abanoub.medhat.seif@gmail.com
-          </p>
-        </div>
-        <div className="py-5 border-b-2 border-gray-200">
-          {menuItems.map((item) => (
-            <div
-              key={item.title}
-              className="flex gap-3 hover:bg-gray-200 active:bg-gray-300 p-5 transition-all duration-200 rounded-xl cursor-pointer"
-              onClick={() => {
-                setActivepanel(item.title);
-              }}
-            >
-              <span>{item.icon}</span>
-              <p className=" text-gray-400 ">{t(item.title)}</p>
-            </div>
-          ))}
-        </div>
-        <div>
-          <button className="py-5 text-gray-400 hover:text-gray-500">
-            {t("logout")}
-          </button>
-        </div>
-      </div>
+      <SidePanel
+        setActivePanel={setActivepanel}
+        menuItems={menuItems}
+      ></SidePanel>
       {/* Right Panel */}
       <div className="bg-gray-200 flex-1 p-10">
         {menuItems.map((item) =>
